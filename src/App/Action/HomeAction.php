920fa5cc21c2b92591ad8abf780adf6d73edd1f7
David Leonardo Vergara <david.l.vergara@gmail.com>
2018-11-18 22:26:01 -0500

    query builder

--- src/App/Action/HomeAction.php
+++ src/App/Action/HomeAction.php
@@ -36,7 +36,7 @@ class HomeAction implements ServerMiddlewareInterface
      */
     public function __construct(Router\RouterInterface $router, Template\TemplateRendererInterface $template = null, EntityManager $entityManager, ContainerInterface $container)
     {
-        $this->router   = $router;
+        $this->router = $router;
         $this->template = $template;
         $this->entityManager = $entityManager;
         $this->container = $container;
@@ -50,14 +50,10 @@ class HomeAction implements ServerMiddlewareInterface
     public function process(ServerRequestInterface $request, DelegateInterface $delegate)
     {
         $config = $this->container->get('config');
-        $fecha = $this->getFecha($request);
-        $order = ['motorcycle_id_motorcycle' => "ASC",'procesed_date_time' => "DESC",];
-        $where = [];
-        if( !empty($fecha) ) {
-            //$where['procesed_date_time'] = $fecha;
-        }
-        $messages = $this->entityManager->getRepository('App\Entity\GeoLog')
-            ->findBy($where, $order, 60);
+        $fecha = $this->getAttr($request, 'fecha');
+        $fechaFin = $this->getAttr($request, 'fechaFin');
+
+        $messages = $this->getData($fecha, $fechaFin);
 
         $data = [];
         $data['geo_log'] = $messages;
@@ -70,17 +66,56 @@ class HomeAction implements ServerMiddlewareInterface
      * @param ServerRequestInterface $request
      * @return string
      */
-    public function getFecha(ServerRequestInterface $request)
+    public function getAttr(ServerRequestInterface $request, $paramName)
     {
         $fecha = "";
-        $fechaUrl = $request->getAttribute("fecha");
+        $fechaUrl = $request->getAttribute($paramName);
         $fechaQuery = $request->getQueryParams();
-        if( empty($fechaUrl) ) {
-            if ( isset($fechaQuery['fecha']) ) {
-                $fecha = $fechaQuery['fecha'];
+        if (empty($fechaUrl)) {
+            if (isset($fechaQuery[$paramName])) {
+                $fecha = $fechaQuery[$paramName];
             }
         }
 
         return $fecha;
+    }
+
+    /**
+     * @param $fecha
+     * @param $fechaInicio
+     * @return array
+     */
+    public function getData($fecha, $fechaFin)
+    {
+        $alias = 't';
+        $order = ['motorcycle_id_motorcycle' => "ASC", 'procesed_date_time' => "DESC",];
+
+        $qb = $this->entityManager->createQueryBuilder();
+        $qb->select($alias)
+            ->from('App\Entity\GeoLog', $alias)
+            ->where($alias . '.latitude IS NOT NULL');
+
+        if (!empty($fecha) && !empty($fechaFin)) {
+            $qb->andWhere($alias . '.procesed_date_time BETWEEN :fecha AND :fechaFin');
+            $qb->setParameter('fecha', $fecha);
+            $qb->setParameter('fechaFin', $fechaFin);
+        } else {
+            if (!empty($fecha)) {
+                $qb->andWhere($alias . '.procesed_date_time >= :fecha');
+                $qb->setParameter('fecha', $fecha);
+            }
+            if (!empty($fechaFin)) {
+                $qb->andWhere($alias . '.procesed_date_time <= :fechaFin');
+                $qb->setParameter('fechaFin', $fechaFin);
+            }
+        }
+
+        foreach ($order as $key => $info) {
+            $qb->addOrderBy($alias . '.' . $key, $info);
+        }
+        
+        $messages = $qb->getQuery()->getResult();
+
+        return $messages;
     }
 }
